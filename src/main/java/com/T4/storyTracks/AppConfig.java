1e613e25226f5411178fc6716e74794e2f03a56d
kiimnj <1najin@naver.com>
2025-01-04 08:30:10 -0800

    gemini+jpa

---
+++ src/main/java/com/T4/storyTracks/AppConfig.java
@@ -0,0 +1,32 @@
+package com.T4.storyTracks;
+
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class AppConfig {
+    String baseUrl = "https://generativelanguage.googleapis.com";
+    String apiKey = "AIzaSyB9pHdfKH_1euOpv1kPrfepDUBoeoMtGwQ";
+    @Bean
+    public RestClient geminiRestClient(String baseUrl,
+                                       String apiKey) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader("x-goog-api-key", apiKey)
+                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Accept", "application/json")
+                .build();
+    }
+
+    @Bean
+    public GeminiInterface geminiInterface(@Qualifier("geminiRestClient") RestClient client) {
+        RestClientAdapter adapter = RestClientAdapter.create(client);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+        return factory.createClient(GeminiInterface.class);
+    }
+}
