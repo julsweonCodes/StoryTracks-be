812ed96c47c2064a89607aa1fe9ca62f9aa1b286
wwbrdle <ww.brdle@gmail.com>
2025-01-14 23:32:58 -0800

    Help me - transmission files

--- src/main/java/com/T4/storyTracks/config/WebConfig.java
+++ src/main/java/com/T4/storyTracks/config/WebConfig.java
@@ -13,8 +13,13 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+<<<<<<< HEAD
                 registry.addMapping("/api/**") // 모든 URL에 대해 CORS 허용
                         .allowedOrigins("http://localhost:3000", "https://storytracks.net") // 허용할 도메인 추가
+=======
+                registry.addMapping("/**") // 모든 URL에 대해 CORS 허용
+                        .allowedOrigins("http://localhost:3000", "http://example.com") // 허용할 도메인 추가
+>>>>>>> 435ba72 (Help me - transmission files)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 쿠키/인증 정보 허용
