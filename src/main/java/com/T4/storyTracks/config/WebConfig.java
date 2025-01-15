e0cac7f1df990151b08921401be3ec940641a159
wwbrdle <ww.brdle@gmail.com>
2025-01-15 12:45:52 -0800

    Edited pipeline etc

--- src/main/java/com/T4/storyTracks/config/WebConfig.java
+++ src/main/java/com/T4/storyTracks/config/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 URL에 대해 CORS 허용
-                        .allowedOrigins("http://localhost:3000", "http://example.com") // 허용할 도메인 추가
+                registry.addMapping("/api/**") // 모든 URL에 대해 CORS 허용
+                        .allowedOrigins("http://localhost:3000", "https://storytracks.net") // 허용할 도메인 추가
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 쿠키/인증 정보 허용
