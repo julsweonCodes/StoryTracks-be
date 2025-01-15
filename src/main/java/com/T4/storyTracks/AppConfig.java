177bf5d7f5f032eb434af41c08aef75d0194a0f3
wwbrdle <ww.brdle@gmail.com>
2025-01-15 12:34:26 -0800

    Edited pipeline etc

--- src/main/java/com/T4/storyTracks/AppConfig.java
+++ src/main/java/com/T4/storyTracks/AppConfig.java
@@ -4,6 +4,7 @@ import lombok.Value;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -17,10 +18,37 @@ public class AppConfig {
 
     @Bean
     public RestClient geminiRestClient() {
-        Properties properties = new Properties();
+
+        try{
+            // 환경 변수 읽기
+            String geminiApiKey = System.getenv("GEMINI_API_KEY");
+
+            // null 체크
+            if (geminiApiKey == null) {
+                throw new IllegalArgumentException("Environment variable 'GEMINI_API_KEY' is not set or is null.");
+            } else {
+                return RestClient.builder()
+                        .baseUrl("https://generativelanguage.googleapis.com")
+                        .defaultHeader("x-goog-api-key", System.getenv("GEMINI_API_KEY"))
+                        .defaultHeader("Content-Type", "application/json")
+                        .defaultHeader("Accept", "application/json")
+                        .build();
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            System.err.println("Unexpected error occurred: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+
+
+        /*Properties properties = new Properties();
         try (FileInputStream input = new FileInputStream("config.properties")) {
             properties.load(input);
-            System.out.println(properties.getProperty("GEMINI_API_KEY"));
             return RestClient.builder()
                     .baseUrl("https://generativelanguage.googleapis.com")
                     .defaultHeader("x-goog-api-key", System.getenv("GEMINI_API_KEY"))
@@ -30,7 +58,7 @@ public class AppConfig {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
-        }
+        }*/
     }
 
     @Bean
